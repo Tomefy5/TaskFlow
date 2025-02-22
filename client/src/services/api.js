bf33c208e878067ac5a97e272d5d31e6eb2aa658
tomefy andry tsiresy <tomefyandrytsiresy@gmail.com>
2025-02-22 16:18:20 +0300

    deploy: modify front end urls

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = await axios.create({
-  baseURL: import.meta.env.VITE_APP_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_APP_API_URL || "https://taskflow-backend-asmg.onrender.com/api",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
